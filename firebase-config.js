1b49d93598afb5aa7fd28a7e82291a19a0b8f583
GLN <asaw1991@gmail.com>
2026-09-02 12:27:38 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyD66_ZIACj3eGHpn-HGYfrJHxewthfDoxU",
+  authDomain: "gln-karen-typing-center.firebaseapp.com",
+  databaseURL: "https://gln-karen-typing-center-default-rtdb.firebaseio.com",
+  projectId: "gln-karen-typing-center",
+  storageBucket: "gln-karen-typing-center.firebasestorage.app",
+  messagingSenderId: "250059382069",
+  appId: "1:250059382069:web:f9640cd8f07f1e50e7c07c"
 };
